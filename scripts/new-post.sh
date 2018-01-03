5ecc419c106cd44bb17b74f6767494eb47ec997f
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-03 22:58:09 +0100

    new script to make new posts

---
+++ scripts/new-post.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+TITLE=$1
+
+if [ "$TITLE" == "" ] ; then
+	echo "enter title (with dashes for multiple words)";
+	exit 1
+fi
+
+cp _posts/template.md _posts/$(date +%Y)-$(date +%m)-$(date +%d)-$TITLE.md;
+
+echo "time to write!"
+
+exit 0;
