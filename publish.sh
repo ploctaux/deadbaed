cdde327b42a2188b25240064910746a21c55d524
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-03 21:21:57 +0100

    added a publish script
    so that i wont forget to actually build and publish the blog

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File              : publish.sh
+# Author            : Philippe Loctaux <loctauxphilippe@gmail.com>
+# Date              : 03.01.2018
+# Last Modified Date: 03.01.2018
+# Last Modified By  : Philippe Loctaux <loctauxphilippe@gmail.com>
+
+git push origin master; git push deploy master
